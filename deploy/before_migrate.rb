c2be4c7db7407b269e79855a88d99b7377ab350f
Han Lee <han@afar.com>
2015-04-14 16:34:16 -0700

    get rid of more errors

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -10,6 +10,6 @@ File.open("/etc/chef-custom/dna.json", 'w') do |f|
 end
 
 # Runs application cookbooks
-run "cd #{latest_release}; sudo bundle exec chef-solo -c #{latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
+run "cd #{config.latest_release}; sudo bundle exec chef-solo -c #{config.latest_release}/deploy/solo.rb -j /etc/chef-custom/dna.json"
 
-run "cat #{latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
+run "cat #{config.latest_release}/deploy/chef-stacktrace.out 2> /dev/null || echo 'Chef run successful'"
